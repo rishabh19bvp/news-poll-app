40a91829ea4b464397000cb45cc962a0061f4a34
Siddhantism <siddhant.bhadale1234@gmail.com>
2025-02-08 17:53:35 +0530

    Initialised Angular
    Storing angular base

--- backend/routes/newsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { fetchNews } = require('../controllers/newsController');
-
-const router = express.Router();
-
-// Fetch news and store unique ones
-router.get('/fetch-news', fetchNews);
-
-module.exports = router;
